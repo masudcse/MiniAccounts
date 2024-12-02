9639d40c1ded1ffcd383bafff90ecd14fb6c678d
masudcse <mia.mahmudunnabi@petronas.com>
2024-12-02 18:56:21 +0800

    add db & connectivity with Dashboard

--- Default.aspx
+++ Default.aspx
@@ -2,4 +2,10 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="PageTitle" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+     <h1>Dashboard</h1>
+    <div>
+        
+        <p>Total Income: <asp:Label ID="lblTotalIncome" runat="server" Text="0"></asp:Label></p>
+        <p>Total Expense: <asp:Label ID="lblTotalExpense" runat="server" Text="0"></asp:Label></p>
+    </div>
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,27 @@ namespace MiniAccounts
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadSummary();
+            }
+        }
+        private void LoadSummary()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["NeekashDBConnectionString"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand("GetSummary", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
 
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    lblTotalIncome.Text = reader["TotalIncome"].ToString();
+                    lblTotalExpense.Text = reader["TotalExpense"].ToString();
+                }
+            }
         }
     }
 }
